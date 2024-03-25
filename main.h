d846cee1402d8b93aadd31d5b5f2c5712b8a45e8
Scandere78 <scanderetejpro@gmail.com>
2024-03-25 15:31:42 +0100

    Add main.h

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN.H_
+#define _MAIN.H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ *  Formats de structure - structure qui stocke différents formats
+ *  _printf peut imprimer et leur fonction respective
+ *  definir type args dans le char 
+ *  @f: la fonction qui imprimera le type associé
+*/
+typedef struct formats
+{
+	char type arg;
+	int (*f)(va_list args);
+} format_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
