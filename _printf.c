dfbeb41515abb57f021dd73d3d7ea9af963d4dfb
my-holber <8762@holbertonstudents.com>
2024-03-26 15:57:07 +0100

    fix _printf.c

--- _printf.c
+++ _printf.c
@@ -3,46 +3,46 @@
 #include "main.h"
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    va_start(args, format);
+	int count = 0;
+	va_list args;
+	va_start(args, format);
 
-    for (int i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            putchar(format[i]);
-            count++;
-        }
-        else
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                int c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (format[i] == 's')
-            {
-                char *s = va_arg(args, char *);
-                count += puts(s);
-            }
-            else if (format[i] == '%')
-            {
-                putchar('%');
-                count++;
-            }
-            else
-            {
-                putchar('%');
-                putchar(format[i]);
-                count += 2;
-            }
-        }
-    }
+	for (int i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			putchar(format[i]);
+			count++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				int c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				char *s = va_arg(args, char *);
+				count += puts(s);
+			}
+			else if (format[i] == '%')
+			{
+				putchar('%');
+				count++;
+			}
+			else
+			{
+				putchar('%');
+				putchar(format[i]);
+				count += 2;
+			}
+		}
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return count;
+	return count;
 }
