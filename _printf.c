c4ba4cd58c97f755d65983eb64b8b9e975753719
my-holber <8762@holbertonstudents.com>
2024-03-26 20:32:34 +0100

    Added tabulations and made changes for C89 compatibility

--- _printf.c
+++ _printf.c
@@ -1,13 +1,23 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
+
+/**
+ * Prints a formatted string to standard output.
+ * 
+ * Supported conversion specifiers: 'c', 's', '%'.
+ * 
+ * @param format The format string.
+ * @param ... Optional arguments corresponding to the format.
+ * @return The number of characters printed (excluding the null byte used to end output to strings).
+ */
 int _printf(const char *format, ...)
 {
-	int i;
 	int count = 0;
 	va_list args;
 	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
+
+	for (int i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
