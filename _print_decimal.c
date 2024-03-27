f52ef7fadd11e11c0ca8460cf85b4275e76bebd5
Scandere78 <scanderetejpro@gmail.com>
2024-03-27 15:44:16 +0100

    add man print decimal

---
+++ _print_decimal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _print_decimal - connect print decimal à print recurcive
+ * @r - print_decimal_recursive
+ * Return: r
+ */
+
+int _print_decimal(va_list args)
+{
+	int i = va_arg(args,int);
+	int r = _print_decimal_recurcive(i);
+        return (r);
+}
