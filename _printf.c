16319450435a53b8cbc0cea0c4d6bbf5d1e98bcf
my-holber <8762@holbertonstudents.com>
2024-03-27 14:01:09 +0100

    Added tabulations and made changes for C89 compatibility

--- _printf.c
+++ _printf.c
@@ -1,59 +1,52 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-
+#include "main.h"
 /**
  * Prints a formatted string to standard output.
  * Supported conversion specifiers: 'c', 's', '%'.
- *
  * @param format The format string.
  * @param ... Optional arguments corresponding to the format.
- *
  * @return The number of characters printed (excluding the null byte used to end output to strings).
  */
 int _printf(const char *format, ...)
 {
-        int i;
-        int count = 0;
-        va_list args;
-        va_start(args, format);
+	int i;
+	int count = 0;
+	va_list args;
+	va_start(args, format);
 
-        for (i = 0; format[i] != '\0'; i++)
-        {
-                if (format[i] != '%')
-                {
-                        putchar(format[i]);
-                        count++;
-                }
-                else
-                {
-                        i++;
-                        if (format[i] == 'c')
-                        {
-                                int c = va_arg(args, int);
-                                putchar(c);
-                                count++;
-                        }
-                        else if (format[i] == 's')
-                        {
-                                char *s = va_arg(args, char *);
-                                count += puts(s);
-                        }
-                        else if (format[i] == '%')
-                        {
-                                putchar('%');
-                                count++;
-                        }
-                        else
-                        {
-                                putchar('%');
-                                putchar(format[i]);
-                                count += 2;
-                        }
-                }
-        }
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				count += _print_char(args);
+			}
+			else if (format[i] == 's')
+			{
+				count += print_string(args);
+			}
+			else if (format[i] == '%')
+			{
+				count += _print_modulo(args);
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+	}
 
-        va_end(args);
+	va_end(args);
 
-        return count;
+	return count;
 }
